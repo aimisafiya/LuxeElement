e0f0f78e257cb442eff867081ce2544ea29ea122
aimisafiya <aimi8safiya@gmail.com>
2026-01-30 11:34:52 +0800

    Main Commit

---
+++ nav-active.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const currentPage = window.location.pathname.split("/").pop();
+
+    const navLinks = document.querySelectorAll("nav a");
+
+    navLinks.forEach(link => {
+        const linkPage = link.getAttribute("href");
+
+        if (linkPage === currentPage) {
+            link.classList.add("active");
+        }
+    });
+
+});
